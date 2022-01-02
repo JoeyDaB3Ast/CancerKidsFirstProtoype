76d21b6faf9d21836f91cab9c31a6497725970b7
JoeyDaB3Ast <60456147+JoeyDaB3Ast@users.noreply.github.com>
2022-01-01 21:29:21 -0500

    updated hover on mobile

--- mobile.js
+++ mobile.js
@@ -45,3 +45,10 @@ buttons.forEach(btn => {
   })
 })
 
+$(document).ready(function() {
+  $('.hover').on('touchstart touchend', function(e) {
+      e.preventDefault();
+      $(this).toggleClass('hover_effect');
+  });
+});
+
