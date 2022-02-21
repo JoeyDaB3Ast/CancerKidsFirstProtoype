d55af1718c3127b264af262bc5d8c6a76f217f0f
JoeyDaB3Ast <60456147+JoeyDaB3Ast@users.noreply.github.com>
2022-02-21 18:53:30 -0500

    added animation on scroll

--- mobile.js
+++ mobile.js
@@ -80,3 +80,14 @@ function watchForHover() {
 }
 
 watchForHover()
+
+const scrollElements = document.querySelectorAll(".js-scroll");
+
+const elementInView = (el, dividend = 1) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) / dividend
+  );
+};
